3bb5a70be294d12e9197458c82e8fb16fc742f9c
Arabin1 <mdasaduzzamanarabin@gmail.com>
2022-12-05 01:16:32 +0600

    Estimate populations upto the 2100

--- Demography.c
+++ Demography.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define DEBUG 0
+#define V_INPUT 0
 #define AGE_GROUP 20
+#define ESTIMATED_GROUP 16
 
-const char FILE_NAME[] = "Files/2011_total.txt";
-const int CURRENT_YEAR = 2011;
-const float DEATH_RATE[] = {5.638, 5.553};
+const char FILE_NAME[] = "Files/2020_total.txt";
+const char DEATH_RATE[] = "Files/death_rate.txt";
+const char INFANT_MORTALITY[] = "Files/infant_mortality.txt";
+const int CURRENT_YEAR = 2020;
 long long int CURRENT_POPULATIONS = 0;
 // Here we are assuming that all the people of age
 // 20 to 24 years is a married couple.
 const int STARTING_COUPLE_YEAR = 20;
 
-void loadPopulation(); // initialize populations from the file
+void loadData(); // Load necessary data from file
 int getYear(); // get the estimated year from user
 float getChild(); // get number of estimated child taken by a couple from user
-float getInfantMorality(); // get infant morality rate per 1000 live births
-long long int calculatePopulations(int year, float child, float iF); // calculate populations and return the updated result
+long long int calculatePopulations(int year, float child); // calculate populations and return the updated result
 void updatePopulations(int child); // update the populations
 
 struct Populations {
@@ -24,28 +26,46 @@ struct Populations {
 };
 
 struct Populations populations[AGE_GROUP];
+float death_rates[ESTIMATED_GROUP];
+float infant_mortality[ESTIMATED_GROUP];
 
 int main()
 {
-    loadPopulation();
+    loadData();
+    if (V_INPUT) {
+        puts("Populations....");
+        for(int i = 0; i < AGE_GROUP; i++) {
+            printf("(%d to %d) ---  %d\n", i * 5, (i + 1) * 5 - 1, populations[i].popu);
+        }
+        putchar('\n');
+
+        puts("Death Rates...");
+        for (int i = 0; i < ESTIMATED_GROUP; i++)
+            printf("(%d to %d) -- %.3f\n", CURRENT_YEAR + (i * 5) + 1, CURRENT_YEAR + (i + 1) *5, death_rates[i]);
+        putchar('\n');
+
+        puts("Infant Mortality...");
+        for (int i = 0; i < ESTIMATED_GROUP; i++)
+            printf("(%d to %d) -- %.3f\n", CURRENT_YEAR + (i * 5) + 1, CURRENT_YEAR + (i + 1) *5, infant_mortality[i]);
+        putchar('\n');
+    }
     int year = getYear();
     float child = getChild();
-    float iF = getInfantMorality();
 
-    long long int predictedPopu = calculatePopulations(year - CURRENT_YEAR, child, iF);
+    long long int predictedPopu = calculatePopulations(year - CURRENT_YEAR, child);
 
     printf("\n\n");
 
     printf("Current Populations:   %d\n", CURRENT_POPULATIONS);
     printf("Predicted Populations: %lld\n", predictedPopu);
-    printf("Populations at 2020:   168220000\n");
     printf("Populations increases: %lld\n", predictedPopu - CURRENT_POPULATIONS);
 
     return 0;
 }
 
-void loadPopulation()
+void loadData()
 {
+    // first load the populations
     FILE *fp;
 
     if ((fp = fopen(FILE_NAME, "rb")) == NULL) {
@@ -64,6 +84,43 @@ void loadPopulation()
         CURRENT_POPULATIONS += popu;
         i++;
     }
+    fclose(fp);
+
+    // now load death_rates
+    if ((fp = fopen(DEATH_RATE, "rb")) == NULL) {
+        printf("The %s file is not in the disk\n", DEATH_RATE);
+        exit(1);
+    }
+
+    float death;
+    i = 0;
+    while(!feof(fp)) {
+        int r = 5;
+        float summer = 0;
+        while(r-- && fscanf(fp, "%f", &death) == 1) {
+            summer += death;
+        }
+        death_rates[i++] = summer / 5; // average of 5 years
+    }
+    fclose(fp);
+
+    // now load infant mortality
+    if ((fp = fopen(INFANT_MORTALITY, "rb")) == NULL) {
+        printf("The %s file is not in the disk\n", INFANT_MORTALITY);
+        exit(1);
+    }
+
+    float mortality;
+    i = 0;
+    while(!feof(fp)) {
+        int r = 5;
+        float summer = 0;
+        while(r-- && fscanf(fp, "%f", &mortality) == 1) {
+            summer += mortality;
+        }
+        infant_mortality[i++] = summer / 5; // average of 5 years
+    }
+    fclose(fp);
 }
 
 int getYear()
@@ -73,7 +130,7 @@ int getYear()
     do {
         printf("Enter an estimated year after %d: ", CURRENT_YEAR);
         scanf("%d", &year);
-    } while (year <= CURRENT_YEAR);
+    } while (year <= CURRENT_YEAR || year > 2100);
 
     return year;
 }
@@ -90,30 +147,18 @@ float getChild()
     return child;
 }
 
-float getInfantMorality()
-{
-    float iF;
-
-    do {
-        printf("Enter infant morality (Per 1000 live births): ");
-        scanf("%f", &iF);
-    } while(iF < 0);
-
-    return iF;
-}
-
-long long int calculatePopulations(int year, float child, float iF)
+long long int calculatePopulations(int year, float child)
 {
     // Here we are assuming that all the peoples between
     // 20 to 24 years is a married couple.
     long long int updatedPopu = CURRENT_POPULATIONS, deathPopu = 0;
-    int y = year, i = 0;
+    int y = year, i = 0, j = 0;
 
     int index = STARTING_COUPLE_YEAR / 5;
     while(y >= 5) {
         // half of the total people
         float newChild = (populations[index].popu / 2) * child;
-        newChild -= (iF / 1000.0) * newChild;
+        newChild -= (infant_mortality[j++] / 1000.0) * newChild;
         updatePopulations(newChild);
         updatedPopu += newChild;
         deathPopu = ((updatedPopu * DEATH_RATE[i++]) / 1000);
@@ -125,7 +170,7 @@ long long int calculatePopulations(int year, float child, float iF)
     if (y){
         int newChild = ((y / 5.0) * (populations[index].popu / 2)) * child;
         if (DEBUG) printf("New child: %d\n", newChild);
-        newChild -= (iF / 1000.0) * newChild;
+        newChild -= (infant_mortality[j++] / 1000.0) * newChild;
         updatePopulations(newChild);
         updatedPopu += newChild;
         deathPopu = ((updatedPopu * DEATH_RATE[i++]) / 1000);
